5b1cc6b8392abf1961e87c26977a056d31f0c17a
Otis YII <oyoung21@gmail.com>
2025-03-20 06:40:03 -0400

    Initial Commit

---
+++ FightTheGoodFIght/FightTheGoodFIghtApp.swift
@@ -0,0 +1,17 @@
+//
+//  FightTheGoodFIghtApp.swift
+//  FightTheGoodFIght
+//
+//  Created by Otis Young on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct FightTheGoodFIghtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
